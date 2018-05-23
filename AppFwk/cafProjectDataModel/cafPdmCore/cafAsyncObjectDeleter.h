OPM/ResInsight

#2873 New AsyncPointerVectorDeleter-classes to handle memory deletion in separate thread.

Fwk/AppFwk/cafProjectDataModel/cafPdmCore/cafAsyncObjectDeleter.h
@@ -1,0 +1,64 @@
+//##################################################################################################
+//
+//   Custom Visualization Core library
+//   Copyright (C) 2011-2013 Ceetron AS
+//
+//   This library may be used under the terms of either the GNU General Public License or
+//   the GNU Lesser General Public License as follows:
+//
+//   GNU General Public License Usage
+//   This library is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   This library is distributed in the hope that it will be useful, but WITHOUT ANY
+//   WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//   FITNESS FOR A PARTICULAR PURPOSE.
+//
+//   See the GNU General Public License at <<http://www.gnu.org/licenses/gpl.html>>
+//   for more details.
+//
+//   GNU Lesser General Public License Usage
+//   This library is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU Lesser General Public License as published by
+//   the Free Software Foundation; either version 2.1 of the License, or
+//   (at your option) any later version.
+//
+//   This library is distributed in the hope that it will be useful, but WITHOUT ANY
+//   WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//   FITNESS FOR A PARTICULAR PURPOSE.
+//
+//   See the GNU Lesser General Public License at <<http://www.gnu.org/licenses/lgpl-2.1.html>>
+//   for more details.
+//
+//##################################################################################################
+
+#pragma once
+
+namespace caf
+{
+    template<typename T>
+    class AsyncRawPointerVectorDeleter
+    {
+    public:
+        AsyncRawPointerVectorDeleter(std::vector<T*>&& pointerVector);
+        ~AsyncRawPointerVectorDeleter();
+        void start();
+    private:
+        std::vector<T*> pointersToDelete_;
+    };
+
+    template<typename T>
+    class AsyncPdmPointerVectorDeleter
+    {
+    public:
+        AsyncPdmPointerVectorDeleter(std::vector<PdmPointer<T>>&& pdmPointerVector);
+        ~AsyncPdmPointerVectorDeleter();
+        void start();
+    private:
+        std::vector<PdmPointer<T>> pointersToDelete_;
+    };
+}
+
+#include "cafAsyncObjectDeleter.inl"

Fwk/AppFwk/cafProjectDataModel/cafPdmCore/cafAsyncObjectDeleter.inl
@@ -1,0 +1,78 @@
+#include <thread>
+
+namespace caf
+{
+    //--------------------------------------------------------------------------------------------------
+    /// Constructor that takes ownership of the data in the provided vector
+    //--------------------------------------------------------------------------------------------------
+    template<typename T>
+    AsyncRawPointerVectorDeleter<T>::AsyncRawPointerVectorDeleter(std::vector<T*>&& pointerVector)
+        : pointersToDelete_(std::move(pointerVector))
+    {
+    }
+
+    //--------------------------------------------------------------------------------------------------
+    /// Destructor will launch the asynchronous deletion if start() hasn't already been run.
+    //--------------------------------------------------------------------------------------------------
+    template<typename T>
+    AsyncRawPointerVectorDeleter<T>::~AsyncRawPointerVectorDeleter()
+    {
+        if (!pointersToDelete_.empty())
+        {
+            start();
+        }
+    }
+
+    //--------------------------------------------------------------------------------------------------
+    /// Perform deletion of the pointers in a separate thread.    
+    //--------------------------------------------------------------------------------------------------
+    template<typename T>
+    void AsyncRawPointerVectorDeleter<T>::start()
+    {
+        std::thread([](std::vector<T*>&& pointerVector)
+        {
+            for (T* pointerToDelete : pointerVector)
+            {
+                delete pointerToDelete;
+            }
+        }, std::move(pointersToDelete_)).detach();
+    }
+
+
+    //--------------------------------------------------------------------------------------------------
+    /// Constructor that takes ownership of the data in the provided vector
+    //--------------------------------------------------------------------------------------------------
+    template<typename T>
+    AsyncPdmPointerVectorDeleter<T>::AsyncPdmPointerVectorDeleter(std::vector<PdmPointer<T>>&& pdmPointerVector)
+        : pointersToDelete_(std::move(pdmPointerVector))
+    {
+    }
+
+    //--------------------------------------------------------------------------------------------------
+    /// Destructor will launch the asynchronous deletion if start() hasn't already been run.
+    //--------------------------------------------------------------------------------------------------
+    template<typename T>
+    AsyncPdmPointerVectorDeleter<T>::~AsyncPdmPointerVectorDeleter()
+    {
+        if (!pointersToDelete_.empty())
+        {
+            start();
+        }
+    }
+  
+    //--------------------------------------------------------------------------------------------------
+    /// Perform deletion of the pointers in a separate thread.    
+    //--------------------------------------------------------------------------------------------------
+    template<typename T>
+    void AsyncPdmPointerVectorDeleter<T>::start()
+    {
+        std::thread([](std::vector<PdmPointer<T>>&& pointerVector)
+        {
+            for (PdmPointer<T>& pointerToDelete : pointerVector)
+            {
+                delete pointerToDelete.rawPtr();
+            }
+        }, std::move(pointersToDelete_)).detach();
+    }
+
+}

CeetronSolutions/Fwk

AppFwk/cafProjectDataModel/cafPdmCore/cafAsyncObjectDeleter.h
@@ -1,0 +1,64 @@
+//##################################################################################################
+//
+//   Custom Visualization Core library
+//   Copyright (C) 2011-2013 Ceetron AS
+//
+//   This library may be used under the terms of either the GNU General Public License or
+//   the GNU Lesser General Public License as follows:
+//
+//   GNU General Public License Usage
+//   This library is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   This library is distributed in the hope that it will be useful, but WITHOUT ANY
+//   WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//   FITNESS FOR A PARTICULAR PURPOSE.
+//
+//   See the GNU General Public License at <<http://www.gnu.org/licenses/gpl.html>>
+//   for more details.
+//
+//   GNU Lesser General Public License Usage
+//   This library is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU Lesser General Public License as published by
+//   the Free Software Foundation; either version 2.1 of the License, or
+//   (at your option) any later version.
+//
+//   This library is distributed in the hope that it will be useful, but WITHOUT ANY
+//   WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//   FITNESS FOR A PARTICULAR PURPOSE.
+//
+//   See the GNU Lesser General Public License at <<http://www.gnu.org/licenses/lgpl-2.1.html>>
+//   for more details.
+//
+//##################################################################################################
+
+#pragma once
+
+namespace caf
+{
+    template<typename T>
+    class AsyncRawPointerVectorDeleter
+    {
+    public:
+        AsyncRawPointerVectorDeleter(std::vector<T*>&& pointerVector);
+        ~AsyncRawPointerVectorDeleter();
+        void start();
+    private:
+        std::vector<T*> m_pointersToDelete;
+    };
+
+    template<typename T>
+    class AsyncPdmPointerVectorDeleter
+    {
+    public:
+        AsyncPdmPointerVectorDeleter(std::vector<PdmPointer<T>>&& pdmPointerVector);
+        ~AsyncPdmPointerVectorDeleter();
+        void start();
+    private:
+        std::vector<PdmPointer<T>> m_pointersToDelete;
+    };
+}
+
+#include "cafAsyncObjectDeleter.inl"

AppFwk/cafProjectDataModel/cafPdmCore/cafAsyncObjectDeleter.inl
@@ -1,0 +1,78 @@
+#include <thread>
+
+namespace caf
+{
+    //--------------------------------------------------------------------------------------------------
+    /// Constructor that takes ownership of the data in the provided vector
+    //--------------------------------------------------------------------------------------------------
+    template<typename T>
+    AsyncRawPointerVectorDeleter<T>::AsyncRawPointerVectorDeleter(std::vector<T*>&& pointerVector)
+        : m_pointersToDelete(std::move(pointerVector))
+    {
+    }
+
+    //--------------------------------------------------------------------------------------------------
+    /// Destructor will launch the asynchronous deletion if start() hasn't already been run.
+    //--------------------------------------------------------------------------------------------------
+    template<typename T>
+    AsyncRawPointerVectorDeleter<T>::~AsyncRawPointerVectorDeleter()
+    {
+        if (!m_pointersToDelete.empty())
+        {
+            start();
+        }
+    }
+
+    //--------------------------------------------------------------------------------------------------
+    /// Perform deletion of the pointers in a separate thread.    
+    //--------------------------------------------------------------------------------------------------
+    template<typename T>
+    void AsyncRawPointerVectorDeleter<T>::start()
+    {
+        std::thread([](std::vector<T*>&& pointerVector)
+        {
+            for (T* pointerToDelete : pointerVector)
+            {
+                delete pointerToDelete;
+            }
+        }, std::move(m_pointersToDelete)).detach();
+    }
+
+
+    //--------------------------------------------------------------------------------------------------
+    /// Constructor that takes ownership of the data in the provided vector
+    //--------------------------------------------------------------------------------------------------
+    template<typename T>
+    AsyncPdmPointerVectorDeleter<T>::AsyncPdmPointerVectorDeleter(std::vector<PdmPointer<T>>&& pdmPointerVector)
+        : m_pointersToDelete(std::move(pdmPointerVector))
+    {
+    }
+
+    //--------------------------------------------------------------------------------------------------
+    /// Destructor will launch the asynchronous deletion if start() hasn't already been run.
+    //--------------------------------------------------------------------------------------------------
+    template<typename T>
+    AsyncPdmPointerVectorDeleter<T>::~AsyncPdmPointerVectorDeleter()
+    {
+        if (!m_pointersToDelete.empty())
+        {
+            start();
+        }
+    }
+  
+    //--------------------------------------------------------------------------------------------------
+    /// Perform deletion of the pointers in a separate thread.    
+    //--------------------------------------------------------------------------------------------------
+    template<typename T>
+    void AsyncPdmPointerVectorDeleter<T>::start()
+    {
+        std::thread([](std::vector<PdmPointer<T>>&& pointerVector)
+        {
+            for (PdmPointer<T>& pointerToDelete : pointerVector)
+            {
+                delete pointerToDelete.rawPtr();
+            }
+        }, std::move(m_pointersToDelete)).detach();
+    }
+
+}