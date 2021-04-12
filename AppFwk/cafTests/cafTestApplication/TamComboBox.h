b5d64d5b1e280ef2a0a8435db7bd428be919290a
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2021-04-11 23:44:40 -0700

    Update .clang-tidy and clang-tidy fixes

--- AppFwk/cafTests/cafTestApplication/TamComboBox.h
+++ AppFwk/cafTests/cafTestApplication/TamComboBox.h
@@ -18,21 +18,20 @@ class TamComboBox : public caf::PdmObject
 public:
     TamComboBox();
 
-    virtual QList<caf::PdmOptionItemInfo> calculateValueOptions(const caf::PdmFieldHandle* fieldNeedingOptions,
-                                                                bool*                      useOptionsOnly) override;
+    QList<caf::PdmOptionItemInfo> calculateValueOptions(const caf::PdmFieldHandle* fieldNeedingOptions,
+                                                        bool*                      useOptionsOnly) override;
 
-    virtual void
-        fieldChangedByUi(const caf::PdmFieldHandle* changedField, const QVariant& oldValue, const QVariant& newValue) override;
+    void fieldChangedByUi(const caf::PdmFieldHandle* changedField, const QVariant& oldValue, const QVariant& newValue) override;
 
 private:
     caf::PdmField<QString> m_name;
 
 protected:
-    virtual void defineUiOrdering(QString uiConfigName, caf::PdmUiOrdering& uiOrdering) override;
+    void defineUiOrdering(QString uiConfigName, caf::PdmUiOrdering& uiOrdering) override;
 
-    virtual void defineEditorAttribute(const caf::PdmFieldHandle* field,
-                                       QString                    uiConfigName,
-                                       caf::PdmUiEditorAttribute* attribute) override;
+    void defineEditorAttribute(const caf::PdmFieldHandle* field,
+                               QString                    uiConfigName,
+                               caf::PdmUiEditorAttribute* attribute) override;
 
 private:
     QStringList m_historyItems;
